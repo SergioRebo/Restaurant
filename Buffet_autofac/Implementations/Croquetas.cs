23b766188ccb5648fd9a8f487a5d598ab0637f16
DESKTOP-L00RSOR\Sergio Rebolledo <sergio200796@hotmail.com>
2021-07-11 15:21:34 +0200

    Autofac implemented. Closes #5

---
+++ Buffet_autofac/Implementations/Croquetas.cs
@@ -0,0 +1,17 @@
+﻿using Buffet_autofac.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buffet_autofac.Implementations
+{
+    public class Croquetas : SetService
+    {
+        public void GetFood()
+        {
+            Console.WriteLine("Has cogido unas croquetillas");
+        }
+    }
+}
