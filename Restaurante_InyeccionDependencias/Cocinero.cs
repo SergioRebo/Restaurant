9d8155181fee80c851087a775856616f34b2a5b5
DESKTOP-L00RSOR\Sergio Rebolledo <sergio200796@hotmail.com>
2021-07-11 12:36:09 +0200

    Contract and classes created. Closes #1

---
+++ Restaurante_InyeccionDependencias/Cocinero.cs
@@ -0,0 +1,23 @@
+﻿using Restaurante_InyeccionDependencias.Contrato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante_InyeccionDependencias
+{
+    public class Cocinero
+    {
+        IPlato plato;
+        public Cocinero(IPlato _plato)
+        {
+            this.plato = _plato;
+        }
+
+        public void Cocinar()
+        {
+
+        }
+    }
+}
---
+++ Restaurante_InyeccionDependencias/Platos/Canelones.cs
@@ -0,0 +1,17 @@
+﻿using Restaurante_InyeccionDependencias.Contrato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante_InyeccionDependencias.Platos
+{
+    public class Canelones : IPlato
+    {
+        public void Cocinar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
